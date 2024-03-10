cf19c82055e22afbbc8c9312ad1fa3695ea8d352
akosgamer1000 <nemet.akos123@gmail.com>
2024-03-10 18:11:55 +0100

    fet:Az órarend feltöltése

--- orarend/MainWindow.xaml.cs
+++ orarend/MainWindow.xaml.cs
@@ -18,11 +18,63 @@ namespace orarend
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// <
     public partial class MainWindow : Window
     {
+        
+        List<Osztaly> osz = new List<Osztaly>();
+        Osztaly A = new Osztaly("12A", new List<Nap>( ));
+        Osztaly B = new Osztaly("12B", new List<Nap>());
+        Osztaly C = new Osztaly("12C", new List<Nap>());
+        utility utility = new utility();
+      
         public MainWindow()
         {
             InitializeComponent();
+            betöltés();
         }
+        public void betöltés()
+        {
+            A.OraszamMegadas();
+            B.OraszamMegadas();
+            C.OraszamMegadas();
+            utility.feltöltés(A);
+            utility.feltöltés(B);
+            utility.feltöltés(C);
+            Console.WriteLine("A");
+            foreach (Nap k in A.Orarend)
+            {
+                
+                foreach (var t in k.Orak)
+                {
+                    
+                    Console.WriteLine(t);
+                }
+                Console.WriteLine("______");
+            }
+            Console.WriteLine("B");
+            foreach (Nap k in B.Orarend)
+            {
+
+                foreach (var t in k.Orak)
+                {
+
+                    Console.WriteLine(t);
+                }
+                Console.WriteLine("______");
+            }
+            Console.WriteLine("c");
+            foreach (Nap k in C.Orarend)
+            {
+
+                foreach (var t in k.Orak)
+                {
+
+                    Console.WriteLine(t);
+                }
+                Console.WriteLine("______");
+            }
+        }
+        
     }
 }
---
+++ orarend/utility.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orarend
+{
+    internal class utility
+    {
+
+
+        string classroom = "";
+        List<int> list = new List<int>() {4,4,4,4,4 };
+        List<string> elérhetőtantárgyak;
+   
+
+
+        public void feltöltés(Osztaly s)
+        {
+           elérhetőtantárgyak= new List<string>() { "Irodalom", "Nyelvtan", "Angol", "Tesneveles", "Torténelem", "Informatika" };
+            classroomselector(s);
+            for(int i = 1; i < 6; i++)
+            {
+                Nap u = new Nap(i, napior(list[i-1],s));
+                s.Orarend.Add(u);
+               
+            }
+        }
+
+        public void classroomselector(Osztaly s)
+        {
+            switch (s.Nev)
+            {
+                case "12A":
+                    classroom = "218";
+                    break;
+                case "12B":
+                    classroom = "208";
+                    break;
+                case "12C":
+                    classroom = "202";
+                    break;
+
+            }
+        }
+        public List<Ora> napior(int caunt,Osztaly s)
+        {
+            Random r= new Random();
+            List<Ora> list = new List<Ora>();
+            int randomcaunt = caunt - 1;
+            s.Oraszam["Matematika"]--;
+            list.Add(new Ora("Matematika","tanár",classroom));
+            for( int i = 0;i < randomcaunt; i++)
+            {
+                
+                int rx = r.Next(elérhetőtantárgyak.Count);
+                string kiválaszott = elérhetőtantárgyak[rx];
+                s.Oraszam[kiválaszott]--;
+                list.Add(new Ora(kiválaszott, "tanár", classroom));
+                check(s);
+            }
+
+            return list;
+
+
+        }
+        public void check(Osztaly s)
+        {
+            foreach(KeyValuePair<string, int> kvp in s.Oraszam)
+            {
+                if (kvp.Value == 0)
+                {
+                    elérhetőtantárgyak.Remove(kvp.Key);
+                }
+            }
+           
+        }
+    }
+}
