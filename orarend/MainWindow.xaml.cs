93d3d974c1ecfd250a22bafaa82b5badf3d29237
Pitttike <gabor.wittmann.uzlet@gmail.com>
2024-03-13 12:56:17 +0100

    fet, fix: Orarend betöltése az Orarend ablakba, mainwindow kód refaktorálása

--- orarend/MainWindow.xaml.cs
+++ orarend/MainWindow.xaml.cs
@@ -21,68 +21,32 @@ namespace orarend
     /// <
     public partial class MainWindow : Window
     {
-        Orarend orarend = new Orarend();
-        List<Osztaly> osz = new List<Osztaly>();
-        Osztaly A = new Osztaly("12A", new List<Nap>());
-        Osztaly B = new Osztaly("12B", new List<Nap>());
-        Osztaly C = new Osztaly("12C", new List<Nap>());
-        utility utility = new utility();
+    
+
+      
 
         public MainWindow()
         {
             InitializeComponent();
             betöltés();
         }
+
+
         public void betöltés()
         {
-            A.OraszamMegadas();
-            B.OraszamMegadas();
-            C.OraszamMegadas();
-            utility.feltöltés(A);
-            utility.feltöltés(B);
-            utility.feltöltés(C);
+           
             osztalyokbox.Items.Add("12A");
 			osztalyokbox.Items.Add("12B");
 			osztalyokbox.Items.Add("12C");
             osztalyokbox.SelectedIndex = 0;
 
-			/*Console.WriteLine("A");
-            foreach (Nap k in A.Orarend)
-            {
-                
-                foreach (var t in k.Orak)
-                {
-                    
-                    Console.WriteLine(t);
-                }
-                Console.WriteLine("______");
-            }
-            Console.WriteLine("B");
-            foreach (Nap k in B.Orarend)
-            {
-
-                foreach (var t in k.Orak)
-                {
-
-                    Console.WriteLine(t);
-                }
-                Console.WriteLine("______");
-            }
-            Console.WriteLine("c");
-            foreach (Nap k in C.Orarend)
-            {
-
-                foreach (var t in k.Orak)
-                {
-
-                    Console.WriteLine(t);
-                }
-                Console.WriteLine("______");
-            */
+	
 		}
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public void Button_Click(object sender, RoutedEventArgs e)
         {
+
+            Orarend orarend = new Orarend(osztalyokbox.SelectedItem.ToString());
             orarend.Show();
         }
    
--- orarend/Orarend.xaml.cs
+++ orarend/Orarend.xaml.cs
@@ -19,24 +19,159 @@ namespace orarend
 	/// </summary>
 	public partial class Orarend : Window
 	{
-		public Orarend()
+
+        public string Valasztas { get; set; }
+
+        Osztaly A = new Osztaly("12A", new List<Nap>());
+        Osztaly B = new Osztaly("12B", new List<Nap>());
+        Osztaly C = new Osztaly("12C", new List<Nap>());
+        utility utility = new utility();
+      
+        private string Choice;
+        Grid grid = new Grid
+        {
+            Height = 300,
+            Width = 600,
+
+        };
+
+        public Orarend(string choice) : base()
 		{
 			InitializeComponent();
+            Choice = choice;
+            Betöltés();
 			GridBetoltes();
+            OrarendBetoltese();
 		}
-		void GridBetoltes()
-		{
-			grid.ShowGridLines = true;
-		for (int i = 0; i < 5; i++) 
-			{
-				grid.RowDefinitions.Add(new RowDefinition());
-			
-			}
-			for (int j = 0; j < 8; j++)
-			{
-				grid.ColumnDefinitions.Add(new ColumnDefinition());
-			}
-		}
-		
-	}
+
+
+      
+
+      
+
+        public void Betöltés()
+        {
+            A.OraszamMegadas();
+            B.OraszamMegadas();
+            C.OraszamMegadas();
+            utility.feltöltés(A);
+            utility.feltöltés(B);
+            utility.feltöltés(C);
+        }
+
+        void GridBetoltes()
+        {
+         
+
+            for (int i = 0; i < 8; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+
+            }
+            for (int j = 0; j < 5; j++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+         
+
+
+            Content = grid;
+        }
+
+        void OrarendBetoltese()
+        {
+            int columnCount = 5;
+           
+
+            switch (Choice)
+            {
+                case "12A":
+                    
+                    foreach (Nap k in A.Orarend)
+                    {
+
+                        foreach (var t in k.Orak)
+                        {
+                            TextBlock tb = new TextBlock
+                            {
+                                Text = t.TantargyNev
+                            };
+                            int rowIndex = grid.Children.Count / columnCount;
+                            int columnIndex = grid.Children.Count % columnCount;
+                         
+                            Grid.SetRow(tb, rowIndex);
+                            Grid.SetColumn(tb, columnIndex);
+                            Border border = new Border();
+                            border.BorderBrush = Brushes.Black; 
+                            border.BorderThickness = new Thickness(1);
+                            border.Child = tb;
+                            Grid.SetRow(border, rowIndex);
+                            Grid.SetColumn(border, columnIndex);
+                            grid.Children.Add(border);
+
+                        }
+                    }
+                    break;
+                case "12B":
+
+                    foreach (Nap k in B.Orarend)
+                    {
+
+                        foreach (var t in k.Orak)
+                        {
+                            TextBlock tb = new TextBlock
+                            {
+                                Text = t.TantargyNev
+                            };
+                            int rowIndex = grid.Children.Count / columnCount;
+                            int columnIndex = grid.Children.Count % columnCount;
+                            Grid.SetRow(tb, rowIndex);
+                            Grid.SetColumn(tb, columnIndex);
+                            Border border = new Border();
+                            border.BorderBrush = Brushes.Black;
+                            border.BorderThickness = new Thickness(1);
+                            border.Child = tb;
+                            Grid.SetRow(border, rowIndex);
+                            Grid.SetColumn(border, columnIndex);
+                            grid.Children.Add(border);
+                        }
+                    }
+                    break;
+
+                case "12C":
+
+                    foreach (Nap k in C.Orarend)
+                    {
+
+                        foreach (var t in k.Orak)
+                        {
+                            TextBlock tb = new TextBlock
+                            {
+                                Text = t.TantargyNev
+                            };
+                            int rowIndex = grid.Children.Count / columnCount;
+                            int columnIndex = grid.Children.Count % columnCount;
+                            Grid.SetRow(tb, rowIndex);
+                            Grid.SetColumn(tb, columnIndex);
+                            Border border = new Border();
+                            border.BorderBrush = Brushes.Black;
+                            border.BorderThickness = new Thickness(1);
+                            border.Child = tb;
+                            Grid.SetRow(border, rowIndex);
+                            Grid.SetColumn(border, columnIndex);
+                            grid.Children.Add(border);
+                        }
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+             
+
+           
+        }
+
+    }
 }
